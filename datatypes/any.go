ab802783361685e833fe0c0a1977864dcf34df96
sujit <s.baniya.np@gmail.com>
2024-05-02 22:21:52 +0545

    update: add any type

---
+++ datatypes/any.go
@@ -0,0 +1,31 @@
+package datatypes
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+)
+
+type Any json.RawMessage
+
+// Scan scan value into Jsonb, implements sql.Scanner interface
+func (j *Any) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+
+	result := json.RawMessage{}
+	err := json.Unmarshal(bytes, &result)
+	*j = Any(result)
+	return err
+}
+
+// Value return json value, implement driver.Valuer interface
+func (j Any) Value() (driver.Value, error) {
+	if len(j) == 0 {
+		return nil, nil
+	}
+	return json.RawMessage(j).MarshalJSON()
+}
