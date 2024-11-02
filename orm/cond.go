15e775e2c4adac8f59db3df7b42ee7ce31adbc1e
sujit <s.baniya.np@gmail.com>
2024-11-02 22:24:45 +0545

    feat: add repository support

---
+++ orm/cond.go
@@ -0,0 +1,439 @@
+// Copyright 2018 Huan Du. All rights reserved.
+// Licensed under the MIT license that can be found in the LICENSE file.
+
+package orm
+
+type Condition interface {
+	Build(field string, value ...interface{}) StringBuilder
+}
+
+var (
+	_ Condition = ValueFunc(nil)
+	_ Condition = ValuesFunc(nil)
+	_ Condition = Func(nil)
+)
+
+type (
+	ValueFunc  func(field string, value interface{}) StringBuilder
+	ValuesFunc func(field string, value ...interface{}) StringBuilder
+	Func       func(field string) StringBuilder
+)
+
+func (f ValueFunc) Build(field string, value ...interface{}) StringBuilder {
+	if len(value) == 0 {
+		return newStringBuilder()
+	}
+	return f(field, value[0])
+}
+
+func (f ValuesFunc) Build(field string, value ...interface{}) StringBuilder {
+	return f(field, value...)
+}
+
+func (f Func) Build(field string, value ...interface{}) StringBuilder {
+	return f(field)
+}
+
+// Cond provides several helper methods to build conditions.
+type Cond struct {
+	Args *Args
+}
+
+// NewCond returns a new Cond.
+func NewCond() *Cond {
+	return &Cond{
+		Args: &Args{},
+	}
+}
+
+func Equal(field string, value interface{}) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" = ")
+	return buf
+}
+
+// Equal represents "field = value".
+func (c *Cond) Equal(field string, value interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" = ")
+	buf.WriteString(c.Args.Add(value))
+	return buf.String()
+}
+
+// E is an alias of Equal.
+func (c *Cond) E(field string, value interface{}) string {
+	return c.Equal(field, value)
+}
+
+// EQ is an alias of Equal.
+func (c *Cond) EQ(field string, value interface{}) string {
+	return c.Equal(field, value)
+}
+
+// NotEqual represents "field <> value".
+func NotEqual(field string, value interface{}) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" <> ")
+	return buf
+}
+
+// NotEqual represents "field <> value".
+func (c *Cond) NotEqual(field string, value interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" <> ")
+	buf.WriteString(c.Args.Add(value))
+	return buf.String()
+}
+
+// NE is an alias of NotEqual.
+func (c *Cond) NE(field string, value interface{}) string {
+	return c.NotEqual(field, value)
+}
+
+// NEQ is an alias of NotEqual.
+func (c *Cond) NEQ(field string, value interface{}) string {
+	return c.NotEqual(field, value)
+}
+
+// GreaterThan represents "field > value".
+func GreaterThan(field string, value interface{}) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" > ")
+	return buf
+}
+
+// GreaterThan represents "field > value".
+func (c *Cond) GreaterThan(field string, value interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" > ")
+	buf.WriteString(c.Args.Add(value))
+	return buf.String()
+}
+
+// G is an alias of GreaterThan.
+func (c *Cond) G(field string, value interface{}) string {
+	return c.GreaterThan(field, value)
+}
+
+// GT is an alias of GreaterThan.
+func (c *Cond) GT(field string, value interface{}) string {
+	return c.GreaterThan(field, value)
+}
+
+// GreaterEqualThan represents "field >= value".
+func GreaterEqualThan(field string, value interface{}) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" >= ")
+	return buf
+}
+
+// GreaterEqualThan represents "field >= value".
+func (c *Cond) GreaterEqualThan(field string, value interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" >= ")
+	buf.WriteString(c.Args.Add(value))
+	return buf.String()
+}
+
+// GE is an alias of GreaterEqualThan.
+func (c *Cond) GE(field string, value interface{}) string {
+	return c.GreaterEqualThan(field, value)
+}
+
+// GTE is an alias of GreaterEqualThan.
+func (c *Cond) GTE(field string, value interface{}) string {
+	return c.GreaterEqualThan(field, value)
+}
+
+// LessThan represents "field < value".
+func LessThan(field string, value interface{}) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" < ")
+	return buf
+}
+
+// LessThan represents "field < value".
+func (c *Cond) LessThan(field string, value interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" < ")
+	buf.WriteString(c.Args.Add(value))
+	return buf.String()
+}
+
+// L is an alias of LessThan.
+func (c *Cond) L(field string, value interface{}) string {
+	return c.LessThan(field, value)
+}
+
+// LT is an alias of LessThan.
+func (c *Cond) LT(field string, value interface{}) string {
+	return c.LessThan(field, value)
+}
+
+// LessEqualThan represents "field <= value".
+func LessEqualThan(field string, value interface{}) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" <= ")
+	return buf
+}
+
+// LessEqualThan represents "field <= value".
+func (c *Cond) LessEqualThan(field string, value interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" <= ")
+	buf.WriteString(c.Args.Add(value))
+	return buf.String()
+}
+
+// LE is an alias of LessEqualThan.
+func (c *Cond) LE(field string, value interface{}) string {
+	return c.LessEqualThan(field, value)
+}
+
+// LTE is an alias of LessEqualThan.
+func (c *Cond) LTE(field string, value interface{}) string {
+	return c.LessEqualThan(field, value)
+}
+
+// In represents "field IN (value...)".
+func In(field string, value ...interface{}) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" IN ")
+	return buf
+}
+
+// In represents "field IN (value...)".
+func (c *Cond) In(field string, value ...interface{}) string {
+	vs := make([]string, 0, len(value))
+
+	for _, v := range value {
+		vs = append(vs, c.Args.Add(v))
+	}
+
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" IN (")
+	buf.WriteStrings(vs, ", ")
+	buf.WriteString(")")
+	return buf.String()
+}
+
+// NotIn represents "field NOT IN (value...)".
+func NotIn(field string, value ...interface{}) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" NOT IN ")
+	return buf
+}
+
+// NotIn represents "field NOT IN (value...)".
+func (c *Cond) NotIn(field string, value ...interface{}) string {
+	vs := make([]string, 0, len(value))
+
+	for _, v := range value {
+		vs = append(vs, c.Args.Add(v))
+	}
+
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" NOT IN (")
+	buf.WriteStrings(vs, ", ")
+	buf.WriteString(")")
+	return buf.String()
+}
+
+// Like represents "field LIKE value".
+func Like(field string, value interface{}) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" LIKE ")
+	return buf
+}
+
+// Like represents "field LIKE value".
+func (c *Cond) Like(field string, value interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" LIKE ")
+	buf.WriteString(c.Args.Add(value))
+	return buf.String()
+}
+
+// NotLike represents "field NOT LIKE value".
+func NotLike(field string, value interface{}) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" NOT LIKE ")
+	return buf
+}
+
+// NotLike represents "field NOT LIKE value".
+func (c *Cond) NotLike(field string, value interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" NOT LIKE ")
+	buf.WriteString(c.Args.Add(value))
+	return buf.String()
+}
+
+// IsNull represents "field IS NULL".
+func IsNull(field string) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" IS NULL")
+	return buf
+}
+
+// IsNull represents "field IS NULL".
+func (c *Cond) IsNull(field string) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" IS NULL")
+	return buf.String()
+}
+
+// IsNotNull represents "field IS NOT NULL".
+func IsNotNull(field string) StringBuilder {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" IS NOT NULL")
+	return buf
+}
+
+// IsNotNull represents "field IS NOT NULL".
+func (c *Cond) IsNotNull(field string) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" IS NOT NULL")
+	return buf.String()
+}
+
+// Between represents "field BETWEEN lower AND upper".
+func (c *Cond) Between(field string, lower, upper interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" BETWEEN ")
+	buf.WriteString(c.Args.Add(lower))
+	buf.WriteString(" AND ")
+	buf.WriteString(c.Args.Add(upper))
+	return buf.String()
+}
+
+// NotBetween represents "field NOT BETWEEN lower AND upper".
+func (c *Cond) NotBetween(field string, lower, upper interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" NOT BETWEEN ")
+	buf.WriteString(c.Args.Add(lower))
+	buf.WriteString(" AND ")
+	buf.WriteString(c.Args.Add(upper))
+	return buf.String()
+}
+
+// Or represents OR logic like "expr1 OR expr2 OR expr3".
+func (c *Cond) Or(orExpr ...string) string {
+	buf := newStringBuilder()
+	buf.WriteString("(")
+	buf.WriteStrings(orExpr, " OR ")
+	buf.WriteString(")")
+	return buf.String()
+}
+
+// And represents AND logic like "expr1 AND expr2 AND expr3".
+func (c *Cond) And(andExpr ...string) string {
+	buf := newStringBuilder()
+	buf.WriteString("(")
+	buf.WriteStrings(andExpr, " AND ")
+	buf.WriteString(")")
+	return buf.String()
+}
+
+// Exists represents "EXISTS (subquery)".
+func (c *Cond) Exists(subquery interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString("EXISTS (")
+	buf.WriteString(c.Args.Add(subquery))
+	buf.WriteString(")")
+	return buf.String()
+}
+
+// NotExists represents "NOT EXISTS (subquery)".
+func (c *Cond) NotExists(subquery interface{}) string {
+	buf := newStringBuilder()
+	buf.WriteString("NOT EXISTS (")
+	buf.WriteString(c.Args.Add(subquery))
+	buf.WriteString(")")
+	return buf.String()
+}
+
+// Any represents "field op ANY (value...)".
+func (c *Cond) Any(field, op string, value ...interface{}) string {
+	vs := make([]string, 0, len(value))
+
+	for _, v := range value {
+		vs = append(vs, c.Args.Add(v))
+	}
+
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" ")
+	buf.WriteString(op)
+	buf.WriteString(" ANY (")
+	buf.WriteStrings(vs, ", ")
+	buf.WriteString(")")
+	return buf.String()
+}
+
+// All represents "field op ALL (value...)".
+func (c *Cond) All(field, op string, value ...interface{}) string {
+	vs := make([]string, 0, len(value))
+
+	for _, v := range value {
+		vs = append(vs, c.Args.Add(v))
+	}
+
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" ")
+	buf.WriteString(op)
+	buf.WriteString(" ALL (")
+	buf.WriteStrings(vs, ", ")
+	buf.WriteString(")")
+	return buf.String()
+}
+
+// Some represents "field op SOME (value...)".
+func (c *Cond) Some(field, op string, value ...interface{}) string {
+	vs := make([]string, 0, len(value))
+
+	for _, v := range value {
+		vs = append(vs, c.Args.Add(v))
+	}
+
+	buf := newStringBuilder()
+	buf.WriteString(Escape(field))
+	buf.WriteString(" ")
+	buf.WriteString(op)
+	buf.WriteString(" SOME (")
+	buf.WriteStrings(vs, ", ")
+	buf.WriteString(")")
+	return buf.String()
+}
+
+// Var returns a placeholder for value.
+func (c *Cond) Var(value interface{}) string {
+	return c.Args.Add(value)
+}
---
+++ orm/delete.go
@@ -0,0 +1,211 @@
+// Copyright 2018 Huan Du. All rights reserved.
+// Licensed under the MIT license that can be found in the LICENSE file.
+
+package orm
+
+import (
+	"strconv"
+)
+
+const (
+	deleteMarkerInit injectionMarker = iota
+	deleteMarkerAfterDeleteFrom
+	deleteMarkerAfterWhere
+	deleteMarkerAfterOrderBy
+	deleteMarkerAfterLimit
+)
+
+// NewDeleteBuilder creates a new DELETE builder.
+func NewDeleteBuilder() *DeleteBuilder {
+	return DefaultFlavor.NewDeleteBuilder()
+}
+
+func newDeleteBuilder() *DeleteBuilder {
+	args := &Args{}
+	proxy := &whereClauseProxy{}
+	return &DeleteBuilder{
+		whereClauseProxy: proxy,
+		whereClauseExpr:  args.Add(proxy),
+
+		Cond: Cond{
+			Args: args,
+		},
+		limit:     -1,
+		args:      args,
+		injection: newInjection(),
+	}
+}
+
+// DeleteBuilder is a builder to build DELETE.
+type DeleteBuilder struct {
+	*WhereClause
+	Cond
+
+	whereClauseProxy *whereClauseProxy
+	whereClauseExpr  string
+
+	table       string
+	orderByCols []string
+	order       string
+	limit       int
+
+	args *Args
+
+	injection *injection
+	marker    injectionMarker
+}
+
+var _ Builder = new(DeleteBuilder)
+
+// DeleteFrom sets table name in DELETE.
+func DeleteFrom(table string) *DeleteBuilder {
+	return DefaultFlavor.NewDeleteBuilder().DeleteFrom(table)
+}
+
+// DeleteFrom sets table name in DELETE.
+func (db *DeleteBuilder) DeleteFrom(table string) *DeleteBuilder {
+	db.table = Escape(table)
+	db.marker = deleteMarkerAfterDeleteFrom
+	return db
+}
+
+// Where sets expressions of WHERE in DELETE.
+func (db *DeleteBuilder) Where(andExpr ...string) *DeleteBuilder {
+	if db.WhereClause == nil {
+		db.WhereClause = NewWhereClause()
+	}
+
+	db.WhereClause.AddWhereExpr(db.args, andExpr...)
+	db.marker = deleteMarkerAfterWhere
+	return db
+}
+
+func (db *DeleteBuilder) WhereCondition(cond Condition, field string, value ...interface{}) *DeleteBuilder {
+	if db.WhereClause == nil {
+		db.WhereClause = NewWhereClause()
+	}
+
+	andExpr := cond.Build(field, value...)
+	pl := make([]string, 0, len(value))
+	for _, v := range value {
+		pl = append(pl, db.args.Add(v))
+	}
+	if len(value) > 1 {
+		andExpr.WriteString("(")
+	}
+	andExpr.WriteStrings(pl, ", ")
+	if len(value) > 1 {
+		andExpr.WriteString(")")
+	}
+	db.WhereClause.AddWhereExpr(db.args, andExpr.String())
+	db.marker = deleteMarkerAfterWhere
+	return db
+}
+
+// AddWhereClause adds all clauses in the whereClause to SELECT.
+func (db *DeleteBuilder) AddWhereClause(whereClause *WhereClause) *DeleteBuilder {
+	if db.WhereClause == nil {
+		db.WhereClause = NewWhereClause()
+	}
+
+	db.WhereClause.AddWhereClause(whereClause)
+	return db
+}
+
+// OrderBy sets columns of ORDER BY in DELETE.
+func (db *DeleteBuilder) OrderBy(col ...string) *DeleteBuilder {
+	db.orderByCols = col
+	db.marker = deleteMarkerAfterOrderBy
+	return db
+}
+
+// Asc sets order of ORDER BY to ASC.
+func (db *DeleteBuilder) Asc() *DeleteBuilder {
+	db.order = "ASC"
+	db.marker = deleteMarkerAfterOrderBy
+	return db
+}
+
+// Desc sets order of ORDER BY to DESC.
+func (db *DeleteBuilder) Desc() *DeleteBuilder {
+	db.order = "DESC"
+	db.marker = deleteMarkerAfterOrderBy
+	return db
+}
+
+// Limit sets the LIMIT in DELETE.
+func (db *DeleteBuilder) Limit(limit int) *DeleteBuilder {
+	db.limit = limit
+	db.marker = deleteMarkerAfterLimit
+	return db
+}
+
+// String returns the compiled DELETE string.
+func (db *DeleteBuilder) String() string {
+	s, _ := db.Build()
+	return s
+}
+
+// Build returns compiled DELETE string and args.
+// They can be used in `DB#Query` of package `database/sql` directly.
+func (db *DeleteBuilder) Build() (sql string, args []interface{}) {
+	return db.BuildWithFlavor(db.args.Flavor)
+}
+
+// BuildWithFlavor returns compiled DELETE string and args with flavor and initial args.
+// They can be used in `DB#Query` of package `database/sql` directly.
+func (db *DeleteBuilder) BuildWithFlavor(flavor Flavor, initialArg ...interface{}) (sql string, args []interface{}) {
+	buf := newStringBuilder()
+	db.injection.WriteTo(buf, deleteMarkerInit)
+
+	if len(db.table) > 0 {
+		buf.WriteLeadingString("DELETE FROM ")
+		buf.WriteString(db.table)
+	}
+
+	db.injection.WriteTo(buf, deleteMarkerAfterDeleteFrom)
+
+	if db.WhereClause != nil {
+		db.whereClauseProxy.WhereClause = db.WhereClause
+		defer func() {
+			db.whereClauseProxy.WhereClause = nil
+		}()
+
+		buf.WriteLeadingString(db.whereClauseExpr)
+		db.injection.WriteTo(buf, deleteMarkerAfterWhere)
+	}
+
+	if len(db.orderByCols) > 0 {
+		buf.WriteLeadingString("ORDER BY ")
+		buf.WriteStrings(db.orderByCols, ", ")
+
+		if db.order != "" {
+			buf.WriteRune(' ')
+			buf.WriteString(db.order)
+		}
+
+		db.injection.WriteTo(buf, deleteMarkerAfterOrderBy)
+	}
+
+	if db.limit >= 0 {
+		buf.WriteLeadingString("LIMIT ")
+		buf.WriteString(strconv.Itoa(db.limit))
+
+		db.injection.WriteTo(buf, deleteMarkerAfterLimit)
+	}
+
+	return db.args.CompileWithFlavor(buf.String(), flavor, initialArg...)
+}
+
+// SetFlavor sets the flavor of compiled sql.
+func (db *DeleteBuilder) SetFlavor(flavor Flavor) (old Flavor) {
+	old = db.args.Flavor
+	db.args.Flavor = flavor
+	return
+}
+
+// SQL adds an arbitrary sql to current position.
+func (db *DeleteBuilder) SQL(sql string) *DeleteBuilder {
+	db.injection.SQL(db.marker, sql)
+	return db
+}
